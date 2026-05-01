b2c99534faed1fc2e52210b6c42b44d72893d9b3
Alrhafi <147037946+Alrhafi@users.noreply.github.com>
2026-05-01 19:35:46 +0700

    Add files via upload

---
+++ Al-Rhafi Pertemuan 1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.alrhafijava;
+
+/**
+ *
+ * @author Hype AMD
+ */
+public class AlrhafiJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        for (int i=0;i<=10;i++){
+            System.out.println("Hello World!");
+        }
+    }
+}
